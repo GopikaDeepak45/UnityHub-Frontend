92615e88f98d421bf3be82b461f282461597f1ab
Gopika Deepak <gopikanath45@gmail.com>
2024-04-25 16:42:02 +0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div >
+      <Button className="text-3xl">Click me</Button>
+    </div>
+  )
+}
+
+export default App
